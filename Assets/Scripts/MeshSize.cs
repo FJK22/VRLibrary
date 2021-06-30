56bc972cd5a1312a98646ab56020d780cffee96e
GlobalMan-coder <68968348+GlobalMan-coder@users.noreply.github.com>
2021-07-01 03:12:56 +0800

    meshsize fix

--- Assets/Scripts/MeshSize.cs
+++ Assets/Scripts/MeshSize.cs
@@ -1,36 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MeshSize : MonoBehaviour
 {
-   
-    public Vector3 sizeofRow;
-    public Vector3 sizeofBook;
-    public MeshRenderer rendererRow;
-    public  MeshRenderer rendererBook;
+    [HideInInspector] public static int totalBookCount = 100; // by other script set this befor start function excute
+    [SerializeField] private Transform floor0;
+    [SerializeField] private Transform floor1;
+    [SerializeField] private Transform CenterOrLibrary;
 
-
-    float noOfBooks;
-    float distanceBetweenBooks = Mathf.Sqrt((5.59f-5.48f) * (5.59f-5.48f) + (0.97f-0.8f) * (0.97f-0.8f));
-
+    [SerializeField] private Transform BookSlotPrefab;
+    [SerializeField] private Transform BookSlotParent;
     
-
-    void Start()
+    [Range(0.1f, 0.5f)]
+    public float distanceOfBook = 1f;
+    public float radius = 0;
+    
+    void Awake()
     {
-        //we get the size of row
-         sizeofRow = rendererRow.bounds.size;
-         Debug.Log("The size of the row is:" + sizeofRow);
-         
-         //we get the size of book
-         sizeofBook = rendererBook.bounds.size;
-         Debug.Log("The size of the book is:" + sizeofBook);
-
-         noOfBooks = Mathf.Sqrt(sizeofRow.x * sizeofRow.x + sizeofRow.z * sizeofRow.z) / distanceBetweenBooks;
-         int noOfBooks2 = (int) noOfBooks;
-
-         Debug.Log("Number of books that this row can take is:" + noOfBooks2);
         
-    }
+        if (totalBookCount == 0) return;
+        Vector3 _center = CenterOrLibrary.position;
+        Vector3 _radiusVector = floor0.GetChild(0).position - _center;
+        radius = Mathf.Sqrt(_radiusVector.x * _radiusVector.x + _radiusVector.z * _radiusVector.z);
+        
+        int _count = 0; // book count
+        Bounds _bound;
+        float _length;
+        int _capacity;
+        float _angleDelta;
+        Vector3 _direction;
+        foreach (Transform row in floor0)
+        {
+            _bound = row.GetComponent<MeshRenderer>().bounds;
+            _length = Mathf.Sqrt(_bound.size.x * _bound.size.x + _bound.size.y * _bound.size.y);
+            Debug.Log(_bound.size);
+            _capacity = (int)(_length / distanceOfBook);
+            Debug.Log(_capacity);
+            _angleDelta = Mathf.Asin(_length / radius / 2f) / (float)_capacity * 360 / Mathf.PI;
+            Debug.Log(_angleDelta);
 
+            for(int i = 0; i < _capacity; i++)
+            {
+                Debug.Log(i);
+                Transform slot = Instantiate(BookSlotPrefab, BookSlotParent);
+                slot.name = row.name + "book" + i;
+                slot.position = row.position;
+                slot.RotateAround(_center, Vector3.up, _angleDelta * i);
+                _direction = new Vector3(_center.x - slot.position.x, 0, _center.z - slot.position.z);
+                slot.rotation = Quaternion.LookRotation(_direction);
+                _count++;
+                if (_count >= totalBookCount) return;
+            }
+        }
+    }
 }
